45c03f89e2e4feface489e4f7442b7b3704a639d
joegoggin <joegoggin27@gmail.com>
2025-05-27 16:41:42 -0400

    Add Note Page

--- client/src/app/note/[noteId]/page.tsx
+++ client/src/app/note/[noteId]/page.tsx
@@ -1,3 +1,6 @@
+import { Note } from "@/types/db/note";
+import { get } from "@/utils/axios";
+
 type NotePageProps = {
     params: Promise<{
         noteId: string;
@@ -6,8 +9,14 @@ type NotePageProps = {
 
 const NotePage: React.FC<NotePageProps> = async ({ params }) => {
     const { noteId } = await params;
+    const { data } = await get<Note>({ route: `/note/${noteId}` });
 
-    return <h1>{noteId}</h1>;
+    return (
+        <div>
+            <h1>{data?.title}</h1>
+            <p>{data?.content}</p>
+        </div>
+    );
 };
 
 export default NotePage;
